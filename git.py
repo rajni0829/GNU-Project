5586d6798c49e821329ee9e1e1cab46e117b7f16
unknown <softygupta2908@gmail.com>
2020-10-04 23:33:35 +0530

    doubts

--- git.py
+++ git.py
@@ -2,5 +2,6 @@ print("Heyy,it's my first github code")
 ques = int(input("Did you used github before ? : "))
 if ques == "yes":
     print("Keep learning !")
+    print "Ask if any doubt"
 else:
     print("Welcome To basics of github !")
