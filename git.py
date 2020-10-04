636f4f1e94c6b8c67300e1a001ae64a661b28267
unknown <softygupta2908@gmail.com>
2020-10-04 19:35:37 +0530

    added git prompt

--- git.py
+++ git.py
@@ -1,1 +1,6 @@
 print("Heyy,it's my first github code")
+ques = int(input("Did you used github before ? : "))
+if ques == "yes":
+    print("Keep learning !")
+else:
+    print("Welcome To basics of github !")
